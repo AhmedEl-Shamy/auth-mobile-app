d92245876e7f56b4793ea5bb3424c619c77127f9
Ahmed Osama <ahmedosama14620@gmail.com>
2024-07-10 06:43:25 +0300

    finish data layer

--- lib/features/authentication/data/repositories/auth_repo_impl.dart
+++ lib/features/authentication/data/repositories/auth_repo_impl.dart
@@ -1,5 +1,5 @@
-
 import 'package:auth_mobile_app/core/utils/failure.dart';
+import 'package:auth_mobile_app/features/authentication/data/data%20sources/auth_local_data_source.dart';
 import 'package:auth_mobile_app/features/authentication/data/data%20sources/auth_remote_data_source.dart';
 import 'package:auth_mobile_app/features/authentication/domain/entities/user_entity.dart';
 import 'package:auth_mobile_app/features/authentication/domain/repositories/auth_repo.dart';
@@ -8,43 +8,46 @@ import 'package:dio/dio.dart';
 
 class AuthRepoImpl extends AuthRepo {
   final AuthRemoteDataSource _authRemoteDataSource;
+  final AuthLocalDataSource _authLocalDataSource;
 
   AuthRepoImpl({
     required AuthRemoteDataSource authRemoteDataSource,
-  }) : _authRemoteDataSource = authRemoteDataSource;
+    required AuthLocalDataSource authLocalDataSource,
+  })  : _authRemoteDataSource = authRemoteDataSource,
+        _authLocalDataSource = authLocalDataSource;
 
   @override
   Future<Either<Failure, UserEntity>> logIn(Map<String, String> params) async {
     try {
       UserEntity user = await _authRemoteDataSource.logIn(params);
       return right(user);
-    } on DioException catch(e) {
+    } on DioException catch (e) {
       return left(ServerFailure.fromDioException(e));
     }
   }
 
   @override
-  Future<Either<Failure, UserEntity>> logInWithToken(Map<String, String> params) async {
+  Future<Either<Failure, UserEntity>> logInWithToken(
+      Map<String, String> params) async {
     try {
       UserEntity user = await _authRemoteDataSource.logInWithToken(params);
       return right(user);
-    } on DioException catch(e) {
+    } on DioException catch (e) {
       return left(ServerFailure.fromDioException(e));
     }
   }
 
   @override
   Future<void> logOut() {
-    // TODO: implement logOut
-    throw UnimplementedError();
+    return _authLocalDataSource.deleteUser();
   }
-  
+
   @override
   Future<Either<Failure, bool>> register(Map<String, String> params) async {
     try {
       await _authRemoteDataSource.register(params);
       return right(true);
-    } on DioException catch(e) {
+    } on DioException catch (e) {
       return left(ServerFailure.fromDioException(e));
     }
   }
